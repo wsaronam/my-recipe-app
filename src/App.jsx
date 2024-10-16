fffaafef88eaf1515e604266c30e54841af0464f
Willy Saronamihardja <wsaronam@gmail.com>
2024-10-16 00:53:12 -0700

    Updated program
    - created recipe cards component
    - added styling to component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import { getRecipeData } from "./API.jsx"
 import { RecipeTable } from './components/RecipeTable.jsx';
-
-
-
+import { RecipeCard } from './components/RecipeCard.jsx';
 
 
 
@@ -13,6 +11,7 @@ import { RecipeTable } from './components/RecipeTable.jsx';
 export default function App() {
   const inputRef = useRef(null);
   const [recipes, setRecipes] = useState([]); // store JSON recipe data
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault(); 
@@ -21,6 +20,8 @@ export default function App() {
     try {
       getRecipeData(userInput).then(data => {
         setRecipes(data); // store the recipe data in recipes state
+        setIsSubmitted(true);
+        console.log(data);  // DELETE THIS LATER
       });
     }
     catch (error) {
@@ -37,13 +38,16 @@ export default function App() {
             <input id="userInput" ref={inputRef} type="text" /><br />
             <button type="submit">Find recipes!</button>
         </form>
-        {recipes.length > 0 && (
+        {isSubmitted && recipes.length > 0 && (
+          <RecipeCard props={recipes[0]} />
+        )}
+        {/* {recipes.length > 0 && (
         <ul>
           {recipes.map(recipe => (
             <li key={recipe.id}>{recipe.title}</li>
           ))}
         </ul>
-      )}
+        )} */}
     </div>
   );
 }
---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function RecipeCard(props) {
+    console.log(props);
+    return (
+        <div className="card">
+            <p className="card-name">{props.props.title}</p>
+            <img className="card-img" src={props.props.image} alt="" />
+            <h3>Ingredients:</h3>
+            <ul>
+                {props.props.usedIngredients.map((ingredient) => (
+                <li key={ingredient.id}>
+                    {ingredient.original}
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
